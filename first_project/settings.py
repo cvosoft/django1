426c30e22e8fefe5d7e86c25928c421ef8d224a6
Christoph Völker <christov@gmx.de>
2025-01-31 08:38:55 +0100

    update

--- first_project/settings.py
+++ first_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5%^7fyidcdm%n6s%+vzc!gm_et*9to*vzp(97$_qr=t-=e0417'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
